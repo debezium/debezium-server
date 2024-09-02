ce1154b7c54e67154b2f2edc81c12d0ebd40c90c
Kunal Bhatnagar <kunbha@raysearchlabs.com>
2024-09-02 13:32:06 +0200

    DBZ-8193 - Added batch write to Aws Kinesis

--- debezium-server-kinesis/src/main/java/io/debezium/server/kinesis/KinesisChangeConsumer.java
+++ debezium-server-kinesis/src/main/java/io/debezium/server/kinesis/KinesisChangeConsumer.java
@@ -7,6 +7,7 @@ package io.debezium.server.kinesis;
 
 import java.net.URI;
 import java.time.Duration;
+import java.util.ArrayList;
 import java.util.List;
 import java.util.Optional;
 
@@ -39,6 +40,10 @@ import software.amazon.awssdk.services.kinesis.KinesisClient;
 import software.amazon.awssdk.services.kinesis.KinesisClientBuilder;
 import software.amazon.awssdk.services.kinesis.model.KinesisException;
 import software.amazon.awssdk.services.kinesis.model.PutRecordRequest;
+import software.amazon.awssdk.services.kinesis.model.PutRecordsRequest;
+import software.amazon.awssdk.services.kinesis.model.PutRecordsRequestEntry;
+import software.amazon.awssdk.services.kinesis.model.PutRecordsResponse;
+import software.amazon.awssdk.services.kinesis.model.PutRecordsResultEntry;
 
 /**
  * Implementation of the consumer that delivers the messages into Amazon Kinesis destination.
@@ -61,6 +66,7 @@ public class KinesisChangeConsumer extends BaseChangeConsumer implements Debeziu
     private Optional<String> endpointOverride;
     private Optional<String> credentialsProfile;
     private static final int DEFAULT_RETRIES = 5;
+    private static final int batchSize = 500;
     private static final Duration RETRY_INTERVAL = Duration.ofSeconds(1);
 
     @ConfigProperty(name = PROP_PREFIX + "null.key", defaultValue = "default")
@@ -106,20 +112,92 @@ public class KinesisChangeConsumer extends BaseChangeConsumer implements Debeziu
     @Override
     public void handleBatch(List<ChangeEvent<Object, Object>> records, RecordCommitter<ChangeEvent<Object, Object>> committer)
             throws InterruptedException {
-        for (ChangeEvent<Object, Object> record : records) {
-            LOGGER.trace("Received event '{}'", record);
 
-            int attempts = 0;
-            while (!recordSent(record)) {
-                attempts++;
-                if (attempts >= DEFAULT_RETRIES) {
-                    throw new DebeziumException("Exceeded maximum number of attempts to publish event " + record);
+        // Split the records into batches of size 500
+        String streamName = records.get(0).destination();
+        List<List<ChangeEvent<Object, Object>>> batchRecords = batchList(records, batchSize);
+        // Process each batch to PutRecordsRequestEntry
+        for (List<ChangeEvent<Object, Object>> batch : batchRecords) {
+            List<PutRecordsRequestEntry> putRecordsRequestEntryList = new ArrayList<>();
+            for (ChangeEvent<Object, Object> record : batch) {
+
+                Object rv = record.value();
+                if (rv == null) {
+                    rv = "";
                 }
-                Metronome.sleeper(RETRY_INTERVAL, Clock.SYSTEM).pause();
+                PutRecordsRequestEntry putRecordsRequestEntry = PutRecordsRequestEntry.builder().partitionKey((record.key() != null) ? getString(record.key()) : nullKey)
+                        .data(SdkBytes.fromByteArray(getBytes(rv))).build();
+                putRecordsRequestEntryList.add(putRecordsRequestEntry);
             }
-            committer.markProcessed(record);
+
+            // Handle Error
+            boolean notSuccesfull = true;
+            int attempts = 0;
+            List<PutRecordsRequestEntry> batchRequest = putRecordsRequestEntryList;
+
+            while (notSuccesfull) {
+
+                if (attempts >= DEFAULT_RETRIES) {
+                    throw new DebeziumException("Exceeded maximum number of attempts to publish event");
+                }
+
+                try {
+                    PutRecordsResponse response = recordsSent(batchRequest, streamName);
+                    attempts++;
+                    if (response.failedRecordCount() > 0) {
+                        Metronome.sleeper(RETRY_INTERVAL, Clock.SYSTEM).pause();
+
+                        final List<PutRecordsResultEntry> putRecordsResults = response.records();
+                        List<PutRecordsRequestEntry> failedRecordsList = new ArrayList<>();
+
+                        for (int index = 0; index < putRecordsResults.size(); index++) {
+                            PutRecordsResultEntry entryResult = putRecordsResults.get(index);
+                            if (entryResult.errorCode() != null) {
+                                failedRecordsList.add(putRecordsRequestEntryList.get(index));
+                            }
+                        }
+                        batchRequest = failedRecordsList;
+
+                    }
+                    else {
+                        notSuccesfull = false;
+                    }
+
+                }
+                catch (KinesisException exception) {
+                    LOGGER.warn("Failed to send record to {}", streamName, exception);
+                    attempts++;
+                    Metronome.sleeper(RETRY_INTERVAL, Clock.SYSTEM).pause();
+                }
+            }
+
+            for (ChangeEvent<Object, Object> record : batch) {
+                committer.markProcessed(record);
+            }
         }
+
+        // Mark Batch Finished
         committer.markBatchFinished();
+    }
+
+    private List<List<ChangeEvent<Object, Object>>> batchList(List<ChangeEvent<Object, Object>> inputList, final int maxSize) {
+        List<List<ChangeEvent<Object, Object>>> batches = new ArrayList<>();
+        final int size = inputList.size();
+        for (int i = 0; i < size; i += maxSize) {
+            batches.add(new ArrayList<>(inputList.subList(i, Math.min(size, i + maxSize))));
+        }
+        return batches;
+    }
+
+    private PutRecordsResponse recordsSent(List<PutRecordsRequestEntry> putRecordsRequestEntryList, String streamName) {
+
+        // Create a PutRecordsRequest
+        PutRecordsRequest putRecordsRequest = PutRecordsRequest.builder().streamName(streamNameMapper.map(streamName)).records(putRecordsRequestEntryList).build();
+
+        // Send Request
+        PutRecordsResponse putRecordsResponse = client.putRecords(putRecordsRequest);
+        LOGGER.trace("Response Receieved: " + putRecordsResponse);
+        return putRecordsResponse;
     }
 
     private boolean recordSent(ChangeEvent<Object, Object> record) {
