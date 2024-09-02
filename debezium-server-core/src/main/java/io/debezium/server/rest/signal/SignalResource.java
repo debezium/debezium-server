775e98e7e4560976b09cbd6a7d336cbef2b4c740
Jakub Cechacek <jcechace@gmail.com>
2024-09-02 15:14:07 +0200

    DBZ-8101 Initial implementation of signal REST API for DS

---
+++ debezium-server-core/src/main/java/io/debezium/server/rest/signal/SignalResource.java
@@ -0,0 +1,39 @@
+/*
+ * Copyright Debezium Authors.
+ *
+ * Licensed under the Apache Software License version 2.0, available at http://www.apache.org/licenses/LICENSE-2.0
+ */
+package io.debezium.server.rest.signal;
+
+import jakarta.inject.Inject;
+import jakarta.validation.constraints.NotNull;
+import jakarta.ws.rs.POST;
+import jakarta.ws.rs.Path;
+import jakarta.ws.rs.core.Response;
+
+import io.debezium.engine.DebeziumEngine;
+import io.debezium.server.DebeziumServer;
+
+@Path("/signals")
+public class SignalResource {
+
+    @Inject
+    DebeziumServer server;
+
+    @POST
+    public Response post(@NotNull DSSignal dsSignal) {
+        var signaler = server.getSignaler();
+        if (signaler == null) {
+            return Response.status(Response.Status.SERVICE_UNAVAILABLE).build();
+        }
+
+        var signal = toSignal(dsSignal);
+        signaler.signal(signal);
+        return Response.accepted().build();
+    }
+
+    private DebeziumEngine.Signal toSignal(DSSignal dsSignal) {
+        return new DebeziumEngine.Signal(dsSignal.id(), dsSignal.type(), dsSignal.data(), dsSignal.additionalData());
+    }
+
+}
