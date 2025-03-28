eab001a3253ae71a9ac3f87cbae84e25cbd7b429
Chris Cranford <chris@hibernate.org>
2025-03-28 15:51:01 -0400

    DBZ-8637 Use configuration-driven mappings

--- debezium-server-instructlab/src/main/java/io/debezium/server/instructlab/InstructLabSinkConsumer.java
+++ debezium-server-instructlab/src/main/java/io/debezium/server/instructlab/InstructLabSinkConsumer.java
@@ -6,29 +6,39 @@
 package io.debezium.server.instructlab;
 
 import java.io.IOException;
+import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
+import java.util.Objects;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+import java.util.stream.Collectors;
+import java.util.stream.Stream;
 
 import jakarta.annotation.PostConstruct;
 import jakarta.annotation.PreDestroy;
 import jakarta.enterprise.context.Dependent;
-import jakarta.inject.Inject;
 import jakarta.inject.Named;
 
+import org.apache.kafka.connect.data.Struct;
 import org.apache.kafka.connect.header.Header;
-import org.apache.kafka.connect.header.Headers;
+import org.apache.kafka.connect.source.SourceRecord;
+import org.eclipse.microprofile.config.Config;
+import org.eclipse.microprofile.config.ConfigProvider;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
 import io.debezium.DebeziumException;
+import io.debezium.annotation.Immutable;
+import io.debezium.annotation.VisibleForTesting;
+import io.debezium.data.Envelope;
 import io.debezium.embedded.EmbeddedEngineChangeEvent;
 import io.debezium.engine.ChangeEvent;
 import io.debezium.engine.DebeziumEngine.ChangeConsumer;
 import io.debezium.engine.DebeziumEngine.RecordCommitter;
 import io.debezium.server.BaseChangeConsumer;
-import io.debezium.server.DebeziumServer;
-import io.debezium.server.instructlab.transforms.AbstractAddQnaHeader;
 import io.debezium.util.Strings;
 
 /**
@@ -44,11 +54,40 @@ public class InstructLabSinkConsumer extends BaseChangeConsumer
 
     private static final Logger LOGGER = LoggerFactory.getLogger(InstructLabSinkConsumer.class);
 
-    @Inject
-    DebeziumServer server;
+    private static final String CONF_PREFIX = "debezium.sink.instructlab.";
+    private static final String TAXONOMY_PREFIX = CONF_PREFIX + "taxonomy.";
+    private static final String TAXONOMY_BASE_PATH = CONF_PREFIX + "taxonomy.base.path";
+    private static final String TAXONOMIES = CONF_PREFIX + "taxonomies";
+
+    private final List<TaxonomyMapping> mappings = new ArrayList<>();
 
     @PostConstruct
     void configure() {
+        final Config config = ConfigProvider.getConfig();
+        final String taxonomyBasePath = config.getValue(TAXONOMY_BASE_PATH, String.class);
+
+        final String[] taxonomyNames = config.getValue(TAXONOMIES, String.class).split(",");
+        for (String taxonomyName : taxonomyNames) {
+            // Read mapping question, answer, and optional context configs
+            final MappingValue question = MappingValue.from(config.getValue(TAXONOMY_PREFIX + taxonomyName + ".question", String.class));
+            final MappingValue answer = MappingValue.from(config.getValue(TAXONOMY_PREFIX + taxonomyName + ".answer", String.class));
+            final MappingValue context = config.getOptionalValue(TAXONOMY_PREFIX + taxonomyName + ".context", String.class)
+                    .map(MappingValue::from)
+                    .orElse(null);
+
+            final String topicRegEx = config.getOptionalValue(TAXONOMY_PREFIX + taxonomyName + ".topic", String.class).orElse(".*");
+
+            // Compute mapping qna.yml filename from taxonomy domain and base paths
+            final String fileName = createTaxonomyQnAPath(taxonomyBasePath,
+                    config.getValue(TAXONOMY_PREFIX + taxonomyName + ".domain", String.class));
+
+            LOGGER.info("Configured taxonomy mapping '{}' to taxonomy {}", taxonomyName, fileName);
+            mappings.add(new TaxonomyMapping(taxonomyName, question, answer, context, Pattern.compile(topicRegEx), fileName));
+        }
+
+        if (mappings.isEmpty()) {
+            throw new DebeziumException("No taxonomy mappings configured.");
+        }
     }
 
     @PreDestroy
@@ -58,21 +97,33 @@ public class InstructLabSinkConsumer extends BaseChangeConsumer
     @Override
     public void handleBatch(List<ChangeEvent<Object, Object>> records, RecordCommitter<ChangeEvent<Object, Object>> committer)
             throws InterruptedException {
+        LOGGER.trace("Processing batch of {} events.", records.size());
         // Read each event and build file batch data
         final Map<String, QnaFile> batchFiles = new HashMap<>();
         for (ChangeEvent<Object, Object> record : records) {
             if (record.value() != null) {
-                getQnaMap(record).forEach((key, attributes) -> {
-                    final String fileName = attributes.get(AbstractAddQnaHeader.ATTRIBUTE_FILENAME);
-                    if (!Strings.isNullOrEmpty(fileName)) {
-                        final String question = attributes.get(AbstractAddQnaHeader.ATTRIBUTE_QUESTION);
-                        final String answer = attributes.get(AbstractAddQnaHeader.ATTRIBUTE_ANSWER);
-                        if (!Strings.isNullOrEmpty(question) && !Strings.isNullOrEmpty(answer)) {
-                            final QnaFile file = batchFiles.computeIfAbsent(fileName, QnaFile::new);
-                            file.addSeedExample(question, answer, attributes.get(AbstractAddQnaHeader.ATTRIBUTE_CONTEXT));
-                        }
+                for (TaxonomyMapping mapping : mappings) {
+                    // Filter by topic
+                    final Matcher matcher = mapping.topicPattern().matcher(record.destination());
+                    if (!matcher.matches()) {
+                        LOGGER.trace("Skipped event for taxonomy {}, topic {} did not match.", mapping.name(), record.destination());
+                        continue;
                     }
-                });
+
+                    LOGGER.trace("Processing taxonomy {} for topic {}", mapping.name(), record.destination());
+
+                    final String question = getChangeMappingValue(record, mapping.question());
+                    final String answer = getChangeMappingValue(record, mapping.answer());
+                    if (!Strings.isNullOrEmpty(question) && !Strings.isNullOrEmpty(answer)) {
+                        final QnaFile file = batchFiles.computeIfAbsent(mapping.fileName(), QnaFile::new);
+                        final String context = getChangeMappingValue(record, mapping.context());
+                        LOGGER.trace("Adding seed example to taxonomy file '{}'", mapping.fileName());
+                        file.addSeedExample(question, answer, context);
+                    }
+                    else {
+                        LOGGER.trace("Cannot add seed example for taxonomy file '{}', question or answer is empty.", mapping.fileName());
+                    }
+                }
             }
             committer.markProcessed(record);
         }
@@ -91,25 +142,109 @@ public class InstructLabSinkConsumer extends BaseChangeConsumer
         committer.markBatchFinished();
     }
 
-    private Map<String, Map<String, String>> getQnaMap(ChangeEvent<Object, Object> record) {
-        final Map<String, Map<String, String>> headers = new HashMap<>();
-        EmbeddedEngineChangeEvent<Object, Object, Object> event = (EmbeddedEngineChangeEvent<Object, Object, Object>) record;
-        Headers kafkaHeaders = event.sourceRecord().headers();
-        for (Header header : kafkaHeaders) {
-            final String key = header.key();
-            if (key.startsWith(AbstractAddQnaHeader.QNA_PREFIX)) {
-                String[] keyParts = key.split("\\.", 3);
-                if (keyParts.length == 3) {
-                    final String prefix = keyParts[1];
-                    final String attribute = keyParts[2];
-                    headers.computeIfAbsent(prefix, k -> new HashMap<>()).put(attribute, String.valueOf(header.value()));
-                }
-                else {
-                    LOGGER.debug("Header key '{}' did not have the expected format of 'qna.<prefix>.<attribute>', skipping.", key);
+    /**
+     * Reads a specific mapping value reference as a String from the change event.
+     *
+     * @param changeEvent the change event, should not be {@code null}
+     * @param mappingValue the mapping value, may be {@code null}
+     * @return the mapping value reference or {@code null} if not found
+     */
+    private String getChangeMappingValue(ChangeEvent<Object, Object> changeEvent, MappingValue mappingValue) {
+        if (mappingValue != null) {
+            final SourceRecord record = getSourceRecord(changeEvent);
+            if (mappingValue.isHeader()) {
+                for (Header header : record.headers()) {
+                    if (header.key().equals(mappingValue.getValue())) {
+                        return String.valueOf(header.value());
+                    }
                 }
             }
+            else if (mappingValue.isField()) {
+                if (record.valueSchema() != null && Envelope.isEnvelopeSchema(record.valueSchema())) {
+                    // Debezium event
+                    final Struct after = ((Struct) record.value()).getStruct(Envelope.FieldName.AFTER);
+                    if (after != null && after.schema().field(mappingValue.getValue()) != null) {
+                        return String.valueOf(after.get(mappingValue.getValue()));
+                    }
+                }
+                else if (record.valueSchema() != null) {
+                    // Flattened event
+                    final Struct struct = (Struct) record.value();
+                    if (struct != null && struct.schema().field(mappingValue.getValue()) != null) {
+                        return String.valueOf(struct.get(mappingValue.getValue()));
+                    }
+                }
+            }
+            else if (mappingValue.isConstant()) {
+                return mappingValue.getValue();
+            }
         }
-        return headers;
+        return null;
     }
 
+    private SourceRecord getSourceRecord(ChangeEvent<Object, Object> record) {
+        return ((EmbeddedEngineChangeEvent<Object, Object, Object>) record).sourceRecord();
+    }
+
+    private String createTaxonomyQnAPath(String basePath, String domain) {
+        return Stream.concat(Stream.concat(Stream.of(basePath), Arrays.stream(domain.split("/"))), Stream.of("qna.yml")).collect(Collectors.joining("/"));
+    }
+
+    /**
+     * Defines a taxonomy configuration-based mapping
+     */
+    @Immutable
+    private record TaxonomyMapping(String name, MappingValue question, MappingValue answer, MappingValue context, Pattern topicPattern, String fileName) {
+
+    }
+
+    /**
+     * A mapping value represents an encoded representation for a taxonomy mapping attribute.
+     */
+    @Immutable
+    @VisibleForTesting
+    public static class MappingValue {
+
+        private final static String FIELD_PREFIX = "value:";
+        private final static String HEADER_PREFIX = "header:";
+
+        private final boolean header;
+        private final boolean field;
+        private final boolean constant;
+        private final String value;
+
+        private MappingValue(boolean header, boolean field, boolean constant, String value) {
+            this.header = header;
+            this.field = field;
+            this.constant = constant;
+            this.value = value;
+        }
+
+        public boolean isHeader() {
+            return header;
+        }
+
+        public boolean isField() {
+            return field;
+        }
+
+        public boolean isConstant() {
+            return constant;
+        }
+
+        public String getValue() {
+            return value;
+        }
+
+        static MappingValue from(String mapping) {
+            Objects.requireNonNull(mapping, "The mapping must not be null or empty");
+            if (mapping.startsWith(FIELD_PREFIX)) {
+                return new MappingValue(false, true, false, mapping.substring(mapping.indexOf(":") + 1));
+            }
+            else if (mapping.startsWith(HEADER_PREFIX)) {
+                return new MappingValue(true, false, false, mapping.substring(mapping.indexOf(":") + 1));
+            }
+            return new MappingValue(false, false, true, mapping);
+        }
+    }
 }
--- debezium-server-instructlab/src/test/java/io/debezium/server/instructlab/InstructLabTestConfigSource.java
+++ debezium-server-instructlab/src/test/java/io/debezium/server/instructlab/InstructLabTestConfigSource.java
@@ -5,12 +5,15 @@
  */
 package io.debezium.server.instructlab;
 
+import java.nio.file.Path;
 import java.util.HashMap;
 import java.util.Map;
 
 import org.apache.kafka.connect.runtime.standalone.StandaloneConfig;
+import org.apache.kafka.connect.transforms.HeaderFrom;
 
 import io.debezium.server.TestConfigSource;
+import io.debezium.transforms.ExtractNewRecordState;
 import io.debezium.util.Testing;
 
 /**
@@ -21,8 +24,21 @@ import io.debezium.util.Testing;
 public class InstructLabTestConfigSource extends TestConfigSource {
 
     public InstructLabTestConfigSource() {
+        final Path taxonomyPath = Testing.Files.createTestingPath("taxonomies");
+
         final Map<String, String> configMap = new HashMap<>();
         configMap.put("debezium.sink.type", "instructlab");
+        configMap.put("debezium.sink.instructlab.taxonomy.base.path", taxonomyPath.toAbsolutePath().toString());
+        configMap.put("debezium.sink.instructlab.taxonomies", "t1,t2");
+        configMap.put("debezium.sink.instructlab.taxonomy.t1.topic", ".*");
+        configMap.put("debezium.sink.instructlab.taxonomy.t1.question", "header:question");
+        configMap.put("debezium.sink.instructlab.taxonomy.t1.answer", "header:answer");
+        configMap.put("debezium.sink.instructlab.taxonomy.t1.context", "header:context");
+        configMap.put("debezium.sink.instructlab.taxonomy.t1.domain", "t1/a/b/c/");
+        configMap.put("debezium.sink.instructlab.taxonomy.t2.topic", "ilab\\.inventory\\.orders");
+        configMap.put("debezium.sink.instructlab.taxonomy.t2.question", "value:purchaser");
+        configMap.put("debezium.sink.instructlab.taxonomy.t2.answer", "value:product_id");
+        configMap.put("debezium.sink.instructlab.taxonomy.t2.domain", "t2/x/y/z/");
 
         configMap.put("debezium.source.connector.class", "io.debezium.connector.postgresql.PostgresConnector");
         configMap.put("debezium.source." + StandaloneConfig.OFFSET_STORAGE_FILE_FILENAME_CONFIG, OFFSET_STORE_PATH.toAbsolutePath().toString());
@@ -31,10 +47,25 @@ public class InstructLabTestConfigSource extends TestConfigSource {
         configMap.put("debezium.source.schema.include.list", "inventory");
         configMap.put("debezium.source.table.include.list", "inventory.orders");
 
-        configMap.put("debezium.transforms", "fraud");
-        configMap.put("debezium.transforms.fraud.type", FraudInstructLabAddQnaHeader.class.getName());
-        configMap.put("debezium.transforms.fraud.name", "fraud");
-        configMap.put("debezium.transforms.fraud.filename", Testing.Files.createTestingPath("fraud/qna.yml").toString());
+        // Copies field's "question", "answer", and "context" to headers of the same names
+        // The headers are then consumed by the sink
+        configMap.put("debezium.transforms", "flatten,question-to-header,answer-to-header,context-to-header");
+        configMap.put("debezium.transforms.flatten.type", ExtractNewRecordState.class.getName());
+        configMap.put("debezium.transforms.question-to-header.type", HeaderFrom.Value.class.getName());
+        configMap.put("debezium.transforms.question-to-header.fields", "purchaser");
+        configMap.put("debezium.transforms.question-to-header.headers", "question");
+        configMap.put("debezium.transforms.question-to-header.operation", "copy");
+        configMap.put("debezium.transforms.answer-to-header.type", HeaderFrom.Value.class.getName());
+        configMap.put("debezium.transforms.answer-to-header.fields", "product_id");
+        configMap.put("debezium.transforms.answer-to-header.headers", "answer");
+        configMap.put("debezium.transforms.answer-to-header.operation", "copy");
+        configMap.put("debezium.transforms.context-to-header.type", HeaderFrom.Value.class.getName());
+        configMap.put("debezium.transforms.context-to-header.fields", "quantity");
+        configMap.put("debezium.transforms.context-to-header.headers", "context");
+        configMap.put("debezium.transforms.context-to-header.operation", "copy");
+
+        configMap.put("quarkus.log.min-level", "TRACE");
+        configMap.put("quarkus.log.category.\"io.debezium.server.instructlab\".level", "TRACE");
 
         config = configMap;
     }
