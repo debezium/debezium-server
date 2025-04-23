664b83b8128ef665524da4d2646c6b891ed04edb
Vojtech Juranek <vjuranek@redhat.com>
2025-04-23 15:30:38 +0200

    DBZ-8948 Add polling start and stop events

---
+++ debezium-server-core/src/main/java/io/debezium/server/events/PollingStoppedEvent.java
@@ -0,0 +1,14 @@
+/*
+ * Copyright Debezium Authors.
+ *
+ * Licensed under the Apache Software License version 2.0, available at http://www.apache.org/licenses/LICENSE-2.0
+ */
+package io.debezium.server.events;
+
+/**
+ * Fired once the polling loop is finished.
+ *
+ * @author vjuranek
+ */
+public class PollingStoppedEvent {
+}
