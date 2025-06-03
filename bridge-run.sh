0c73c26b63912bb3096e615594712d7a24d196ca
Szonja <szonja.jakab@getbridge.com>
2025-06-03 23:10:34 +0200

    Fix: update pr comments

--- bridge-run.sh
+++ bridge-run.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-export JAVA_OPTS="-agentlib:jdwp=transport=dt_socket,server=y,suspend=n,address=*:5005 -javaagent:jolokia.jar -Dcom.sun.management.jmxremote=true -Dcom.sun.management.jmxremote.authenticate=false -Dcom.sun.management.jmxremote.ssl=false -Dcom.sun.management.jmxremote.port=9012 -Dcom.sun.management.jmxremote.rmi.port=9012 -Djava.rmi.server.hostname=$POD_IP"
+export JAVA_OPTS="-javaagent:jolokia.jar -Dcom.sun.management.jmxremote=true -Dcom.sun.management.jmxremote.authenticate=false -Dcom.sun.management.jmxremote.ssl=false -Dcom.sun.management.jmxremote.port=9012 -Dcom.sun.management.jmxremote.rmi.port=9012 -Djava.rmi.server.hostname=$POD_IP"
 
 if [[ -v PULSAR_SERVICE_ACCOUNT_JSON ]]; then
   echo "$PULSAR_SERVICE_ACCOUNT_JSON" > /tmp/pulsar_creds.json
