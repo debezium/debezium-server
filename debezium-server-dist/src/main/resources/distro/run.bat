4ec9c28da0c45ba965fb9e072646bdec693edf0f
Chris Cranford <chris@hibernate.org>
2025-10-27 12:02:56 -0400

    DBZ-9625 Fix run.bat to start properly

--- debezium-server-dist/src/main/resources/distro/run.bat
+++ debezium-server-dist/src/main/resources/distro/run.bat
@@ -7,5 +7,5 @@ echo %RUNNER%
 SET LIB_PATH=lib\*
 @REM Configuration files and directories that need to be on the classpath
 SET LIB_CONFIG=config\lib
-IF %ENABLE_DEBEZIUM_SCRIPTING%=="true" LIB_PATH=%LIB_PATH%%PATH_SEP%lib_opt\*
+IF "%ENABLE_DEBEZIUM_SCRIPTING%"=="true" LIB_PATH=%LIB_PATH%%PATH_SEP%lib_opt\*
 call "%JAVA_BINARY%" %DEBEZIUM_OPTS% %JAVA_OPTS% -cp %RUNNER%%PATH_SEP%%LIB_CONFIG_PATH%%PATH_SEP%%LIB_PATH% io.debezium.server.Main
