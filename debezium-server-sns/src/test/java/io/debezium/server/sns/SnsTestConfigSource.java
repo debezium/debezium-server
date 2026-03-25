3b0cb2be0a5b00ffd2e50d9ca60493e31534fdec
Rafael Rain <rafaelrainbr@outlook.com>
2026-03-25 19:43:03 -0300

    debezium/dbz#739 Rename snsConfigMap
    Signed-off-by: Rafael Rain <rafaelrainbr@outlook.com>

--- debezium-server-sns/src/test/java/io/debezium/server/sns/SnsTestConfigSource.java
+++ debezium-server-sns/src/test/java/io/debezium/server/sns/SnsTestConfigSource.java
@@ -17,18 +17,18 @@ public class SnsTestConfigSource extends TestConfigSource {
     public static final String SNS_REGION = "us-east-1";
 
     public SnsTestConfigSource() {
-        Map<String, String> sqsConfigMap = new HashMap<>();
+        Map<String, String> snsConfigMap = new HashMap<>();
 
-        sqsConfigMap.put("debezium.sink.type", "sns");
-        sqsConfigMap.put("debezium.sink.sns.region", SNS_REGION);
-        sqsConfigMap.put("debezium.source.connector.class", "io.debezium.connector.postgresql.PostgresConnector");
-        sqsConfigMap.put("debezium.source." + StandaloneConfig.OFFSET_STORAGE_FILE_FILENAME_CONFIG, OFFSET_STORE_PATH.toAbsolutePath().toString());
-        sqsConfigMap.put("debezium.source.offset.flush.interval.ms", "0");
-        sqsConfigMap.put("debezium.source.topic.prefix", "testc");
-        sqsConfigMap.put("debezium.source.schema.include.list", "inventory");
-        sqsConfigMap.put("debezium.source.table.include.list", "inventory.customers");
+        snsConfigMap.put("debezium.sink.type", "sns");
+        snsConfigMap.put("debezium.sink.sns.region", SNS_REGION);
+        snsConfigMap.put("debezium.source.connector.class", "io.debezium.connector.postgresql.PostgresConnector");
+        snsConfigMap.put("debezium.source." + StandaloneConfig.OFFSET_STORAGE_FILE_FILENAME_CONFIG, OFFSET_STORE_PATH.toAbsolutePath().toString());
+        snsConfigMap.put("debezium.source.offset.flush.interval.ms", "0");
+        snsConfigMap.put("debezium.source.topic.prefix", "testc");
+        snsConfigMap.put("debezium.source.schema.include.list", "inventory");
+        snsConfigMap.put("debezium.source.table.include.list", "inventory.customers");
 
-        config = sqsConfigMap;
+        config = snsConfigMap;
     }
 
     @Override
