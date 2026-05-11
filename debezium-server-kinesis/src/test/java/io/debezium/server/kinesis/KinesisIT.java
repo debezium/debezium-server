df0800db470e1c584f6a31047ef5509743749050
kmos <kmos@commonhaus.dev>
2026-05-11 22:13:01 +0200

    debezium/dbz#1731 linting
    Signed-off-by: kmos <kmos@commonhaus.dev>

--- debezium-server-kinesis/src/test/java/io/debezium/server/kinesis/KinesisIT.java
+++ debezium-server-kinesis/src/test/java/io/debezium/server/kinesis/KinesisIT.java
@@ -9,13 +9,13 @@ import java.time.Duration;
 import java.util.ArrayList;
 import java.util.List;
 
-import io.debezium.runtime.events.ConnectorStartedEvent;
-import io.debezium.runtime.events.DebeziumCompletionEvent;
 import jakarta.enterprise.event.Observes;
 
 import org.awaitility.Awaitility;
 import org.junit.jupiter.api.Test;
 
+import io.debezium.runtime.events.ConnectorStartedEvent;
+import io.debezium.runtime.events.DebeziumCompletionEvent;
 import io.debezium.testing.testcontainers.PostgresTestResourceLifecycleManager;
 import io.debezium.util.Testing;
 import io.quarkus.test.common.QuarkusTestResource;
